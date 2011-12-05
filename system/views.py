4aa54da2dd42d067aecc92178637d54b7764f2e9
atlas605 <thanhnx.605@gmail.com>
2011-12-06 00:07:48 +0700

    signout template update

--- system/views.py
+++ system/views.py
@@ -2,17 +2,21 @@
 from django.shortcuts import render_to_response
 from MCS.system.forms import SignInForm, ShopSignUpForm
 from django.http import HttpResponseRedirect
-from models import User
 from MCS.system.models import Shop
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 def index(request):
     """ trang chu"""
-    return render_to_response("index.html")
+    if not request.user.is_authenticated():
+        return render_to_response("index.html")
+
+    return render_to_response("index.html", {"username": request.user.username})
 
 def sign_in(request):
-    """ trang dang nhap
-        user: admin; pass: admin"""
+    """ trang dang nhap"""
+    if request.user.is_authenticated():
+        return HttpResponseRedirect("/usercp/")
+    
     if request.method == "POST":
         form = SignInForm(data=request.POST)
         if form.is_valid():
@@ -27,15 +31,20 @@ def sign_in(request):
                 else:
                     message = user.username + " is blocked"
                     return render_to_response("failure.html", {"message":message})
-            else:
-                message = "invalid login"
-                return render_to_response("failure.html", {"message":message})
     else:
         form = SignInForm()
     return render_to_response("sign_in.html",{"form": form})
 
+def signout(request):
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect("/")
+    
+    logout(request)
+    return render_to_response("sign_out.html")
+
 def shop_signup(request):
     """ cua hang dang ki"""
+
     if request.method == "POST":
         form = ShopSignUpForm(data=request.POST)
         if form.is_valid():
@@ -43,13 +52,14 @@ def shop_signup(request):
             form.save()
             return render_to_response("success.html")
     else:
+        if request.user.is_authenticated():
+            return HttpResponseRedirect("/")
         form = ShopSignUpForm()
     return render_to_response("shop_signup.html",{"form": form})
 
 def admin(request):
     """ trang cua admin"""
-    if not request.user.is_authenticated():
-        
+    if not request.user.is_authenticated() and request.user.is_superuser:
         message = "user is not admin"
         return render_to_response("failure.html",{"message": message})
     shops = Shop.objects.all()
@@ -58,10 +68,5 @@ def admin(request):
 def usercp(request):
     if not request.user.is_authenticated():
         return HttpResponseRedirect("/signin/")
-    """ user control panel site"""
-    print request.user
-    return render_to_response("usercp.html")
 
-def success(request):
-    """ thong bao dang nhap thanh cong"""
-    return render_to_response("success.html")
+    return render_to_response("usercp.html", {"username": request.user.username})
